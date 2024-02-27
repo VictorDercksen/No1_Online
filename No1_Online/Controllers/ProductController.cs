c364d7fcccfa8898eeb03471a501ed278a6ed7ea
VictorDercksen <victordercksen@gmail.com>
2024-02-27 16:26:00 +0200

    Refactor almost complete

---
+++ No1_Online/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace No1_Online.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Product()
+        {
+            return PartialView("Product");
+        }
+    }
+}
